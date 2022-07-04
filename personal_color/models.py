e8c293a12100debb07f9fe48e932d1a9340710c3
class <test>
2022-07-04 14:30:10 +0900

    フォームの追加、アップロードされたファイルの保存先を指定

--- personal_color/models.py
+++ personal_color/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    photo = models.ImageField(upload_to='documents/', default='defo')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    output = models.ImageField(default = 'output/output.jpg')
