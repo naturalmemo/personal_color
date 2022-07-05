366d2bc4c51c6c39499d64b6e0ae8caf02e39a51
class <test>
2022-07-05 16:51:26 +0900

    form.pyに__init__の設定

--- personal_color/forms.py
+++ personal_color/forms.py
@@ -16,5 +16,18 @@ class InquiryForm(forms.Form):
 
     name = forms.CharField(label='お名前', max_length=30)
     email = forms.EmailField(label='メールアドレス')
-    title = forms.ChoiceField(label='タイトル', widget=forms.RadioSelect, choices= CHOICE, required=True)
-    message = forms.CharField(laber='メッセージ', widget=forms.Textarea)
+    title = forms.ChoiceField(label='お問い合わせの種類', widget=forms.RadioSelect, choices= CHOICE, initial=0)
+    message = forms.CharField(label='お問い合わせ内容', widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['class']='form-control'
+        self.fields['name'].widget.attrs['placeholder']='山田 太郎'
+
+        self.fields['email'].widget.attrs['class']='form-control'
+        self.fields['email'].widget.attrs['placeholder']='xxx@email'
+
+        self.fields['title'].widget.attrs['class']='form-control'
+
+        self.fields['message'].widget.attrs['class']='form-control'
