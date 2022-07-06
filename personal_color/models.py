2ad30470692d665054d426422344af33ba54b177
class <test>
2022-07-06 13:29:48 +0900

    写真取り込みテスト

--- personal_color/models.py
+++ personal_color/models.py
@@ -1,6 +1,4 @@
 from django.db import models
 
-# class Document(models.Model):
-#     photo = models.ImageField(upload_to='documents/', default='defo')
-#     uploaded_at = models.DateTimeField(auto_now_add=True)
-#     output = models.ImageField(default = 'output/output.jpg')
+class Sample(models.Model):
+    img = models.ImageField(verbose_name='img', blank=True, null=True)
