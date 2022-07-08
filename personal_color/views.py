f426ccb792e2d2cff9ad67b087dce99f70b7f536
class <test>
2022-07-08 15:25:35 +0900

    genderをフォームに導入途中

--- personal_color/views.py
+++ personal_color/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views import generic
 from django.contrib import messages
-from .forms import InquiryForm, ImgForm
+from .forms import InquiryForm, PersonalForm
 from .models import Sample
 from config.settings import *
 import shutil
@@ -14,26 +14,25 @@ logger = logging.getLogger(__name__)
 
 class IndexView(generic.FormView):
     template_name = "index.html"
-    form_class = ImgForm
+    form_class = PersonalForm
 
     # postメソッドをオーバーライドする
     def post(self, request, *args, **kwargs):
-        form = ImgForm(request.POST)
+        form = PersonalForm(request.POST)
 
         # 前の画像データを消す
         shutil.rmtree(MEDIA_ROOT)
         os.mkdir(MEDIA_ROOT)
 
-        # フォームから受け取った画像データを保存する
+        # フォームから受け取ったデータをmodelのフィールドに格納
         sample = Sample()
         sample.img = request.FILES['img']
+        #sample.gender = request.FILES['gender']
 
         #テストで追加予定
         #sample.save()
 
-        #sample_imgをresult.htmlで表示できるか
-        #削除予定↓
-        sample_img = sample.img
+        img = sample.img
 
         #画像判定モデルの使用
         from personal_color_finder import personal_color_finder
@@ -45,7 +44,7 @@ class IndexView(generic.FormView):
 
 
         #結果をresult.htmlに送ってHTML生成
-        context={"img":sample_img}
+        context={"img":img}
         return render(request, 'result.html', context)
         # {
         #     'form': self.form_class , 
@@ -53,8 +52,6 @@ class IndexView(generic.FormView):
         #     'sample_img': sample_img,
         #     }
         #)
-
-
 
 
 class InquiryView(generic.FormView):
@@ -79,6 +76,7 @@ class LoginView(generic.TemplateView):
 
 class LogoutView(generic.TemplateView):
     template_name = "signup.html"
+
 
 class MembersView(generic.TemplateView):
     template_name = "members.html"
