2fd2611e98e484890de800386d4de6fa52150e1f
program-nishi-iwa-lee <pugoyu@merry.pink>
2022-07-11 14:12:57 +0900

    DB作成中(models.py)onogi

--- personal_color/models.py
+++ personal_color/models.py
@@ -3,6 +3,8 @@ from config.settings import MEDIA_ROOT
 
 from config.settings_common import MEDIA_URL
 
+from django.db import models
+
 CHOICES = [
     (1, '女性'),
     (2, '男性'),
@@ -16,3 +18,25 @@ class Sample(models.Model):
 
     def __str__(self):
         return str(self.gender)
+
+
+
+class Base_type(models.Model):
+    ID = models.IntegerField()
+    NAME = models.CharField(max_length=10)
+    BASE_DOC = models.CharField(max_length=20)
+    
+class Colors(models.Model):
+    ID = models.IntegerField()
+    NAME = models.CharField(max_length=10)
+    BASE_TYPE_ID = models.IntegerField()
+
+class Items(models.Model):
+    ID = models.IntegerField()
+    COLOR_ID = models.IntegerField()
+    SEX = models.IntegerField()
+    NAME = models.CharField(max_length=20)
+    EXPLANAION = models.CharField(max_length=100)
+    PRICE = models.IntegerField()
+    SIZE = models.ImageField()
+    MAKER =  models.CharField(max_length=20)
