b30c545fce9aac8367efda9418eabbb30be618e2
kou47 <ottuntun47@gmail.com>
2022-07-15 06:24:19 +0900

    テーブル作成

--- personal_color/models.py
+++ personal_color/models.py
@@ -1,4 +1,6 @@
+from tabnanny import verbose
 from django.db import models
+from accounts.models import CustomUser
 
 CHOICES = [
     (0, '女性'),
@@ -13,3 +15,45 @@ class Sample(models.Model):
 
     def __str__(self):
         return str(self.gender)
+
+
+class Base_type(models.Model):
+    #ベースタイプテーブルの作成
+    season = models.CharField(max_length=10)
+    base = models.CharField(max_length=10)
+    img_path = models.CharField(max_length=10)
+    base_type_txt = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = 'ベースタイプ'
+
+
+class Colors(models.Model):
+    #色テーブルの作成
+    color = models.CharField(max_length=20)
+    img_path = models.CharField(max_length=10)
+    base_type_id = models.IntegerField()
+
+    def __str__(self):
+        return str(self.color)
+
+    class Meta:
+        verbose_name_plural = '色'
+
+
+class Items(models.Model):
+    #アイテムテーブルの作成
+
+    gender = models.IntegerField()
+    Items_name = models.CharField(max_length=20,null=False)
+    img_path = models.CharField(max_length=10)
+    color_id = models.IntegerField()
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = 'アイテム'
