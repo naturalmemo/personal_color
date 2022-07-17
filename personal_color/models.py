e4090ec8f186f3df65d5b5e0ac95cb61421c8565
kou47 <ottuntun47@gmail.com>
2022-07-17 16:29:04 +0900

    db変更

--- personal_color/models.py
+++ personal_color/models.py
@@ -1,16 +1,16 @@
 from tabnanny import verbose
 from django.db import models
-from accounts.models import CustomUser
+#from accounts.models import CustomUser
 
 CHOICES = [
-    (0, '女性'),
-    (1, '男性'),
-    (2, 'その他'),
-    (3, '無回答')
+    (1, '女性'),
+    (2, '男性'),
+    (3, 'その他'),
+    (4, '無回答')
 ]
 
 class Sample(models.Model):
-    gender = models.CharField(verbose_name='gender', choices=CHOICES, blank=False, null=False, max_length=100, default=0)
+    gender = models.CharField(verbose_name='gender', choices=CHOICES, blank=False, null=False, max_length=100, default=1)
     img = models.ImageField(verbose_name='img', blank=False, null=False)
 
     def __str__(self):
@@ -19,10 +19,10 @@ class Sample(models.Model):
 
 class Base_type(models.Model):
     #ベースタイプテーブルの作成
-    name = models.CharField(max_length=10)
-    base = models.CharField(max_length=10)
-    base_doc = models.CharField(max_length=100)
-    color_doc = models.CharField(max_length=100)
+    name = models.CharField(verbose_name='季節', max_length=10)
+    base = models.CharField(verbose_name='ベースタイプ', max_length=10)
+    base_doc = models.CharField(verbose_name='ベース説明', max_length=100)
+    color_doc = models.CharField(verbose_name='色の説明', max_length=100)
 
     def __str__(self):
         return str(self.name)
@@ -33,9 +33,9 @@ class Base_type(models.Model):
 
 class Colors(models.Model):
     #色テーブルの作成
-    name = models.CharField(max_length=20)
-    path = models.CharField(max_length=30)
-    base_type_id = models.IntegerField()
+    name = models.CharField(verbose_name='色名', max_length=20)
+    img = models.ImageField(verbose_name='色の画像')
+    base_type_id = models.IntegerField(verbose_name='ベースタイプ接続ID')
 
     def __str__(self):
         return str(self.name)
@@ -46,13 +46,13 @@ class Colors(models.Model):
 
 class Items(models.Model):
     #アイテムテーブルの作成
-
-    path = models.CharField(max_length=30)
-    gender = models.IntegerField()
-    color_id = models.IntegerField()
+    name = models.CharField(verbose_name='アイテム名', max_length=20)
+    img = models.ImageField(verbose_name='アイテム画像')
+    gender = models.IntegerField(verbose_name='性別')
+    color_id = models.IntegerField(verbose_name='色テーブル接続ID')
 
     def __str__(self):
-        return str(self.path)
+        return str(self.img)
 
     class Meta:
         verbose_name_plural = 'アイテム'
